7b9de093aa5ad1e3833c13d4a4c583d43fd0bfba
vasuAI <vasu.choubet@appinvetiv.com>
2022-07-06 10:46:02 +0530

    commit

--- src/utils/ScreenNames.tsx
+++ src/utils/ScreenNames.tsx
@@ -1,6 +1,10 @@
 export default {
   HOME: 'Home',
+  BOOKS: 'Books',
   LOG_IN: 'Login',
   SIGN_UP: 'SignUp',
+  WRITING: 'Writing',
+  MY_BOOKS: 'My Books',
+  APPOINTMENTS: 'Appointments',
   BOTTOM_TAB: 'BottomTabNavigator',
 };
