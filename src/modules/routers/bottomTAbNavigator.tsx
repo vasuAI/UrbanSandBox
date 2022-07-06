1434f2800baa354f069c1193ab0bbc8d4bf05df5
vasuAI <vasu.choubet@appinvetiv.com>
2022-07-06 10:45:53 +0530

    bottomTabCreated

--- src/modules/routers/bottomTAbNavigator.tsx
+++ src/modules/routers/bottomTAbNavigator.tsx
@@ -1,6 +1,10 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import HomeScreen from '../home';
 import React from 'react';
+import Discussion from '../discussion/Discussion';
+import ScreenNames from '../../utils/ScreenNames';
+import {Image, StyleSheet} from 'react-native';
+import {LocalImages} from '../../utils';
+import {normalize} from '../../utils/Dimensions';
 const Tab = createBottomTabNavigator();
 
 function BottmTabnavigator() {
@@ -9,10 +13,43 @@ function BottmTabnavigator() {
       screenOptions={{
         headerShown: false,
       }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      {/* <Tab.Screen name="Settings" component={SettingsScreen} /> */}
+      <Tab.Screen
+        name={ScreenNames.HOME}
+        component={Discussion}
+        options={{
+          tabBarStyle: {
+            height: normalize(80),
+            borderTopLeftRadius: normalize(15),
+            borderTopRightRadius: normalize(15),
+          },
+          tabBarIcon: ({focused}) => {
+            return focused ? (
+              <Image
+                source={LocalImages.homeActiveIcon}
+                style={styles.iconStyle}
+              />
+            ) : (
+              <Image
+                source={LocalImages.homeActiveIcon}
+                style={styles.iconStyle}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen name={ScreenNames.BOOKS} component={Discussion} />
+      <Tab.Screen name={ScreenNames.WRITING} component={Discussion} />
+      <Tab.Screen name={ScreenNames.APPOINTMENTS} component={Discussion} />
+      <Tab.Screen name={ScreenNames.MY_BOOKS} component={Discussion} />
     </Tab.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  iconStyle: {
+    width: 30,
+    height: 30,
+  },
+});
 
 export default BottmTabnavigator;
